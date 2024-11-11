a18564f5312d9c6807f627c279d133e27f972e92
Muhammad Raihan Permadi <32005026+rpd-251003@users.noreply.github.com>
2024-11-12 05:55:56 +0700

    .

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -29,9 +29,9 @@
         <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
             <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
                 <a class="navbar-brand brand-logo" href="/../public/dist/index.html"><img
-                        src="{{ asset('dist/dist/assets/images/logo.svg') }}" alt="logo" /></a>
+                        src="{{ asset('dist/assets/images/logo.svg') }}" alt="logo" /></a>
                 <a class="navbar-brand brand-logo-mini" href="/../public/dist/index.html"><img
-                        src="{{ asset('dist/dist/assets/images/logo-mini.svg') }}" alt="logo" /></a>
+                        src="{{ asset('dist/assets/images/logo-mini.svg') }}" alt="logo" /></a>
             </div>
             <div class="navbar-menu-wrapper d-flex align-items-stretch">
                 <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
