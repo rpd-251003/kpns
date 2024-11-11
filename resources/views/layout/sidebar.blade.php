8a8541049211910bd97c6ddc44cbaaaac89bfcb7
Muhammad Raihan Permadi <32005026+rpd-251003@users.noreply.github.com>
2024-11-12 06:06:32 +0700

    .

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,18 +1,32 @@
+@if ($user['role'] == 'member')
 <li class="nav-item">
     <a class="nav-link" href="/dashboard">
-        <span class="menu-title">Dashboard</span>
-        <i class="mdi mdi-home menu-icon"></i>
+        <span class="menu-title">Profile Anggota</span>
+        <i class="mdi mdi-account menu-icon"></i> <!-- Icon Profile -->
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="/dashboard">
+        <span class="menu-title">Bagi Hasil SiPitung</span>
+        <i class="mdi mdi-cash-multiple menu-icon"></i> <!-- Icon Bagi Hasil -->
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="/dashboard">
+        <span class="menu-title">Histori</span>
+        <i class="mdi mdi-history menu-icon"></i> <!-- Icon Histori -->
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="/dashboard">
+        <span class="menu-title">Bagikan Info KPNS</span>
+        <i class="mdi mdi-share-variant menu-icon"></i> <!-- Icon Bagikan Info -->
     </a>
 </li>
 
-@if ($user['role'] == 'member')
 
-Ini Untuk Member
-
-@endif
-<li class="nav-item">
-    <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
-        aria-controls="ui-basic">
+{{-- <li class="nav-item">
+    <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
         <span class="menu-title">Basic UI Elements</span>
         <i class="menu-arrow"></i>
         <i class="mdi mdi-crosshairs-gps menu-icon"></i>
@@ -20,17 +34,16 @@ Ini Untuk Member
     <div class="collapse" id="ui-basic">
         <ul class="nav flex-column sub-menu">
             <li class="nav-item">
-                <a class="nav-link"
-                    href="/../public/dist/pages/ui-features/buttons.html">Buttons</a>
+                <a class="nav-link" href="/../public/dist/pages/ui-features/buttons.html">Buttons</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link"
-                    href="/../public/dist/pages/ui-features/dropdowns.html">Dropdowns</a>
+                <a class="nav-link" href="/../public/dist/pages/ui-features/dropdowns.html">Dropdowns</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link"
-                    href="/../public/dist/pages/ui-features/typography.html">Typography</a>
+                <a class="nav-link" href="/../public/dist/pages/ui-features/typography.html">Typography</a>
             </li>
         </ul>
     </div>
-</li>
+</li> --}}
+
+@endif
