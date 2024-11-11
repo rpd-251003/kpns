f444ba919531251a4f978391c9d01f30bab433fa
Muhammad Raihan Permadi <32005026+rpd-251003@users.noreply.github.com>
2024-11-12 06:00:25 +0700

    .

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -37,7 +37,7 @@
                 <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
                     <span class="mdi mdi-menu"></span>
                 </button>
-                <div class="search-field d-none d-md-block">
+                {{-- <div class="search-field d-none d-md-block">
                     <form class="d-flex align-items-center h-100" action="#">
                         <div class="input-group">
                             <div class="input-group-prepend bg-transparent">
@@ -47,7 +47,7 @@
                                 placeholder="Search projects">
                         </div>
                     </form>
-                </div>
+                </div> --}}
                 <ul class="navbar-nav navbar-nav-right">
                     <li class="nav-item nav-profile dropdown">
                         <a class="nav-link dropdown-toggle" id="profileDropdown" href="#"
@@ -178,7 +178,7 @@
                             <h6 class="p-3 mb-0 text-center">See all notifications</h6>
                         </div>
                     </li>
-                    <li class="nav-item nav-logout d-none d-lg-block">
+                    {{-- <li class="nav-item nav-logout d-none d-lg-block">
                         <a class="nav-link" href="#">
                             <i class="mdi mdi-power"></i>
                         </a>
@@ -187,7 +187,7 @@
                         <a class="nav-link" href="#">
                             <i class="mdi mdi-format-line-spacing"></i>
                         </a>
-                    </li>
+                    </li> --}}
                 </ul>
                 <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
                     data-toggle="offcanvas">
@@ -214,135 +214,9 @@
                             <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/../public/dist/index.html">
-                            <span class="menu-title">Dashboard</span>
-                            <i class="mdi mdi-home menu-icon"></i>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
-                            aria-controls="ui-basic">
-                            <span class="menu-title">Basic UI Elements</span>
-                            <i class="menu-arrow"></i>
-                            <i class="mdi mdi-crosshairs-gps menu-icon"></i>
-                        </a>
-                        <div class="collapse" id="ui-basic">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item">
-                                    <a class="nav-link"
-                                        href="/../public/dist/pages/ui-features/buttons.html">Buttons</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link"
-                                        href="/../public/dist/pages/ui-features/dropdowns.html">Dropdowns</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link"
-                                        href="/../public/dist/pages/ui-features/typography.html">Typography</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="collapse" href="#icons" aria-expanded="false"
-                            aria-controls="icons">
-                            <span class="menu-title">Icons</span>
-                            <i class="mdi mdi-contacts menu-icon"></i>
-                        </a>
-                        <div class="collapse" id="icons">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item">
-                                    <a class="nav-link"
-                                        href="/../public/dist/pages/icons/font-awesome.html">Font Awesome</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="collapse" href="#forms" aria-expanded="false"
-                            aria-controls="forms">
-                            <span class="menu-title">Forms</span>
-                            <i class="mdi mdi-format-list-bulleted menu-icon"></i>
-                        </a>
-                        <div class="collapse" id="forms">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item">
-                                    <a class="nav-link"
-                                        href="/../public/dist/pages/forms/basic_elements.html">Form
-                                        Elements</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="collapse" href="#charts" aria-expanded="false"
-                            aria-controls="charts">
-                            <span class="menu-title">Charts</span>
-                            <i class="mdi mdi-chart-bar menu-icon"></i>
-                        </a>
-                        <div class="collapse" id="charts">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item">
-                                    <a class="nav-link"
-                                        href="/../public/dist/pages/charts/chartjs.html">ChartJs</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="collapse" href="#tables" aria-expanded="false"
-                            aria-controls="tables">
-                            <span class="menu-title">Tables</span>
-                            <i class="mdi mdi-table-large menu-icon"></i>
-                        </a>
-                        <div class="collapse" id="tables">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item">
-                                    <a class="nav-link"
-                                        href="/../public/dist/pages/tables/basic-table.html">Basic table</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false"
-                            aria-controls="auth">
-                            <span class="menu-title">User Pages</span>
-                            <i class="menu-arrow"></i>
-                            <i class="mdi mdi-lock menu-icon"></i>
-                        </a>
-                        <div class="collapse" id="auth">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item">
-                                    <a class="nav-link"
-                                        href="/../public/dist/pages/samples/blank-page.html"> Blank Page </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/../public/dist/pages/samples/login.html">
-                                        Login </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/../public/dist/pages/samples/register.html">
-                                        Register </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/../public/dist/pages/samples/error-404.html">
-                                        404 </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/../public/dist/pages/samples/error-500.html">
-                                        500 </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/../public/dist/docs/documentation.html" target="_blank">
-                            <span class="menu-title">Documentation</span>
-                            <i class="mdi mdi-file-document-box menu-icon"></i>
-                        </a>
-                    </li>
+
+                    @include('layout.sidebar')
+
                 </ul>
             </nav>
             <!-- partial -->
