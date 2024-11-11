d0f36b0047ffaec0f58add7bbddf020cc8f67dd3
Muhammad Raihan Permadi <32005026+rpd-251003@users.noreply.github.com>
2024-11-12 06:09:20 +0700

    new

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -208,8 +208,8 @@
                                 <!--change to offline or busy as needed-->
                             </div>
                             <div class="nav-profile-text d-flex flex-column">
-                                <span class="font-weight-bold mb-2">David Grey. H</span>
-                                <span class="text-secondary text-small">Project Manager</span>
+                                <span class="font-weight-bold mb-2">{{ $user['name'] }}</span>
+                                <span class="text-secondary text-small">{{ $user['role'] }}</span>
                             </div>
                             <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
                         </a>
