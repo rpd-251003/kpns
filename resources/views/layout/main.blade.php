1dfc803a8a07f5f8adc656523eb2d42f43e9425d
Muhammad Raihan Permadi <32005026+rpd-251003@users.noreply.github.com>
2024-11-12 13:38:13 +0700

    .

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -222,9 +222,8 @@
             <!-- partial -->
             <div class="main-panel">
                 <div class="content-wrapper">
+                    @yield('content')
                 </div>
-                <!-- content-wrapper ends -->
-                <!-- partial:../../partials/_footer.html -->
                 <footer class="footer">
                     <div class="d-sm-flex justify-content-center justify-content-sm-between">
                         <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2023 <a
